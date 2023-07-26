87f2200727b70cb5a21acb5b44754367b274aca7
paras-017 <paraschauhan0017@gmail.com>
2023-07-26 20:57:04 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+const App = () => {
+  return (
+    <>
+      <Navbar/>
+    </>
+  )
+}
+
+export default App
