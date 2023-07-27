70c988c0ec0c26a2f852be4dd63403fa0656aec3
paras-017 <paraschauhan0017@gmail.com>
2023-07-27 11:41:52 +0530

    nothing

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="carousel">
+      </div>
+      <div className="offers"></div>
+      <div className="popular-categories"></div>
+      <div className="sponsors"></div>
+      <div className="brand"></div>
+      <div className="pet-category"></div>
+      <div className="veterinary"></div>
+    </div>
+  )
+}
+
+export default Home
