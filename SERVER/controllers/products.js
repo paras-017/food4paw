f157539f01ba76bf57ead919b4602e058800c8a1
paras-017 <paraschauhan0017@gmail.com>
2023-07-31 22:36:37 +0530

    RestApi:pagination added

--- SERVER/controllers/products.js
+++ SERVER/controllers/products.js
@@ -1,22 +1,27 @@
 const Product = require('../models/product')
 
 const getAllproducts = async(req, res)=>{
-    const {company, name, featured, sort} = req.query
-    
+    const {company, name, featured, sort, select} = req.query
     const queryObject = {}
 
     if(company) queryObject.company = company
     if(featured) queryObject.company = featured
-
-    
     if(name) queryObject.name = {$regex:name, $options:'i'}
     
     let apiData = Product.find(queryObject)
     if(sort) apiData = apiData.sort(sort)
+    if (select) {
+        let selectFix = select.split(",").join(" ");
+        apiData = apiData.select(selectFix);
+      }
 
-    
-    const myData = await apiData
-    res.status(200).json({myData})   
+      let page = Number(req.query.page) || 1;
+      let limit = Number(req.query.limit) || 10;
+      let skip = (page - 1) * limit;
+      apiData = apiData.skip(skip).limit(limit);
+   
+      const Products = await apiData;
+      res.status(200).json({ Products, nbHits: Products.length });
 }
 
 module.exports = {getAllproducts}
