dafcb030da7d01261711b6c6b3c3ea4be018a847
paras-017 <paraschauhan0017@gmail.com>
2023-07-31 07:23:44 +0530

    VET:Doctor UI grid used instead of flex

--- src/components/Doctors/Doctor.jsx
+++ src/components/Doctors/Doctor.jsx
@@ -3,13 +3,14 @@ import doctors from './doctorsAPI.js'
 import DoctorCard from './DoctorCard.jsx';
 const Doctor = () => {
   return (
-    <div className='max-w-[85rem] m-auto mt-8'>
-      <div className={`grid grid-cols-1 md:grid-cols-4 md:gap-2  px-3`}>
-        {doctors.map((doctor, index) => (
-          <DoctorCard key={index} docName={doctor.docName} docDesc={doctor.docDesc} docImg={doctor.docImg} />
-        ))}
-      </div>
+    <div className="container mx-auto p-4">
+    <h1 className="text-3xl font-bold mb-4">Our Doctors</h1>
+    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+      {doctors.map((doctor, index) => (
+        <DoctorCard key={index} docName={doctor.docName} docDesc={doctor.docDesc} docImg={doctor.docImg} />
+      ))}
     </div>
+  </div>
   )
 }
 
--- src/components/Doctors/DoctorCard.jsx
+++ src/components/Doctors/DoctorCard.jsx
@@ -3,17 +3,13 @@ import React from 'react';
 const DoctorCard = ({ docName, docDesc, docImg }) => {
   return (
       
-
-
-    
-
-       <div className=" bg-white rounded-lg shadow-lg ] mb-4 w-4rem">
-        <img src={docImg} alt={docName} className="w-full h-40 object-contain" />
-        <div className="px-4 py-4">
-          <h2 className="text-2xl font-bold mb-2">{docName}</h2>
-          <p className="text-gray-600">{docDesc}</p>
-        </div>
-      </div>
+    <div className="bg-white rounded-lg shadow-lg overflow-hidden mb-4">
+    <img src={docImg} alt={docName} className="w-full h-40 object-contain" />
+    <div className="px-4 py-4">
+      <h2 className="text-xl font-bold mb-2">{docName}</h2>
+      <p className="text-gray-600">{docDesc}</p>
+    </div>
+  </div>
 
   );
 };
