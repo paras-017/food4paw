bbb4b72d15f2cdfe9ff0fbd9c6f9e00443021e8b
paras-017 <paraschauhan0017@gmail.com>
2023-07-31 20:18:16 +0530

    RestApi: Add Filtration & Searching Functionality with Query Props

--- SERVER/controllers/products.js
+++ SERVER/controllers/products.js
@@ -1,6 +1,6 @@
 const Product = require('../models/product')
 const getAllproducts = async(req, res)=>{
-    const myData = await Product.find()
+    const myData = await Product.find(req.query)
     res.status(200).json({myData})   
 }
 
