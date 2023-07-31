3000175bbc33b33c38e63b080b0065ff6d6e1ed3
paras-017 <paraschauhan0017@gmail.com>
2023-07-31 22:22:27 +0530

    RestApi: sort functionality added

--- SERVER/controllers/products.js
+++ SERVER/controllers/products.js
@@ -1,18 +1,21 @@
 const Product = require('../models/product')
 
 const getAllproducts = async(req, res)=>{
-    const {company,name} = req.query
+    const {company, name, featured, sort} = req.query
+    
     const queryObject = {}
 
     if(company) queryObject.company = company
     if(featured) queryObject.company = featured
-    //if my name is iphone it'll search all iphone inc. iphone 10
+
+    
     if(name) queryObject.name = {$regex:name, $options:'i'}
     
+    let apiData = Product.find(queryObject)
+    if(sort) apiData = apiData.sort(sort)
 
-    console.log(queryObject)
-    //by default find is querying all the products
-    const myData = await Product.find(queryObject)
+    
+    const myData = await apiData
     res.status(200).json({myData})   
 }
 
