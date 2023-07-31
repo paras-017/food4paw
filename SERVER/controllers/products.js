09270eebafe5412a679977a4fc180b51e33b0b8f
paras-017 <paraschauhan0017@gmail.com>
2023-07-31 21:18:27 +0530

    RestApiv: Add Adv. search functionality in our API

--- SERVER/controllers/products.js
+++ SERVER/controllers/products.js
@@ -1,11 +1,15 @@
 const Product = require('../models/product')
 
 const getAllproducts = async(req, res)=>{
-    const {company} = req.query
+    const {company,name} = req.query
     const queryObject = {}
-    if(company){
-     queryObject.company = company
-    }
+
+    if(company) queryObject.company = company
+    if(featured) queryObject.company = featured
+    //if my name is iphone it'll search all iphone inc. iphone 10
+    if(name) queryObject.name = {$regex:name, $options:'i'}
+    
+
     console.log(queryObject)
     //by default find is querying all the products
     const myData = await Product.find(queryObject)
