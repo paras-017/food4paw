c1e33d15f787d2ff9dfb29e13b44ed1ee4e71f14
paras-017 <paraschauhan0017@gmail.com>
2023-07-31 17:28:42 +0530

    restApi: Stored API data (JSON) in DB

---
+++ SERVER/productDB.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const connectDB = require("./db/connect");
+const Product = require("./models/product");
+const ProductJson = require('./products.json')
+
+const start = async() => {
+    try {
+        await connectDB()
+        await Product.create(ProductJson)
+        console.log('success')
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
