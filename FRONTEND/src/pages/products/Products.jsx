6e0395331359dc532c3331acaddeb86f1f4a2a4c
paras-017 <paraschauhan0017@gmail.com>
2023-08-02 22:00:31 +0530

    API Hosted and tested in frontend using Axios

--- FRONTEND/src/pages/products/Products.jsx
+++ FRONTEND/src/pages/products/Products.jsx
@@ -1,6 +1,26 @@
-import React from 'react'
-
+import React, { useEffect } from 'react'
+import axios from 'axios'
 const Products = () => {
+  
+  
+  useEffect(() => {
+   
+    const fetchData = async () => {
+      const apiUrl =  `https://food4pawsapi-production.up.railway.app/api/products?_id=&brand=&sort=`
+
+      try {
+        const response = await axios.get(apiUrl);
+        const data = response.data
+        console.log(data.Products);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [])
+  
+
   return (
     <div>Products</div>
   )
