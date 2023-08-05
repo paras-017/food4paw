478ffd1ac56396417a249036220302c42fabbe3c
paras-017 <paraschauhan0017@gmail.com>
2023-08-05 12:13:33 +0530

    SingleProduct: data destructured from products

--- FRONTEND/src/pages/singleProduct/SingleProduct.jsx
+++ FRONTEND/src/pages/singleProduct/SingleProduct.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { useProductContext } from '../../context/productContext'
+
+const API = `https://food4pawsapi-production.up.railway.app/api/products`
 
 const SingleProduct = () => {
+  const {getSingleProduct,isSingleLoading,singleProduct}=useProductContext()
+  const {_id}=useParams()
+  const {_id:Pid,name,brand,price,description1,description2,ingredients,stock,stars,review,images} = singleProduct
 
+  useEffect(() => {
+    getSingleProduct(`${API}?_id=${_id}`)
+  }, [])
+  
+  if(isSingleLoading)return<div>Loadding</div>
+      
+  
   
   return (
-    <div>SingleProduct</div>
+    <div className='flex'>
+      <div className="Images">Image-section</div>
+      <div className="Detail">Detail-section</div>
+    </div>
   )
 }
 
